41aef4fdf5e9ba5f1669c325f9dffd65ffe2a8c7
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-01-14 14:54:24 -0600

    added createFile method with 2 params; created writeOnFile method with param; created listContacts method with pararm

--- src/IOJulian.java
+++ src/IOJulian.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 public class IOJulian {
     //    String directory = "data";
@@ -13,7 +15,7 @@ public class IOJulian {
 //    Path dataFile = Paths.get(filename);
 
     //  creating method to create a directory
-    public static void createDirectory(Path directoryName){
+    static void createDirectory(Path directoryName){
         try{
             if(Files.notExists(directoryName)){
                 Files.createDirectory(directoryName);
@@ -24,9 +26,12 @@ public class IOJulian {
     }
 
     //  creating method to create a file
-    public void createFile(Path dataFile){
+    static void createFile(Path directoryName, Path dataFile){
         try{
-            if(Files.notExists(dataFile)){
+            if(Files.notExists(directoryName)){
+                Files.createDirectory(directoryName);
+            }
+            if(!Files.exists(dataFile)){
                 Files.createFile(dataFile);
             }
         }catch (IOException iox){
@@ -34,15 +39,40 @@ public class IOJulian {
         }
     }
 
+    //  method to write in file
+    static void writeOnFile(Path dataFile){
+        try{
+            if(Files.exists(dataFile)){
+                Files.write(dataFile, Arrays.asList("A.J. Brown | 649-555-5555"));
+            }
+        }catch(IOException iox){
+            iox.printStackTrace();
+        }
+    }
+
+    //  method to list contacts
+    static void listContacts(){}
+    static void listContacts(Path fileName){
+    try{
+        List<String> contacts = Files.readAllLines(fileName);
+    }catch(IOException iox){
+        iox.printStackTrace();
+    }
+
+    }
+
     public static void main(String[] args) {
         Input user1 = new Input();
         String directory = user1.getString("Name your Directory");
 //        String directory = "data";
-//        String filename = "contacts.txt";
+        String filename = "contacts.txt";
 
         Path dataDirectory = Paths.get(directory);
-//        Path dataFile = Paths.get(filename);
+        Path dataFile = Paths.get(directory, filename);
 
-        createDirectory(dataDirectory);
+//        createDirectory(dataDirectory);
+        createFile(dataDirectory, dataFile);
+        writeOnFile(dataFile);
+
     }
 }
