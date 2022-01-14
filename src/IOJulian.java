77bf05d6b006c73493decb9ca35981b5b9031100
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-01-14 15:38:59 -0600

    completed writeOnFile, addToFile, listContacts method

--- src/IOJulian.java
+++ src/IOJulian.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,64 +16,83 @@ public class IOJulian {
 //    Path dataFile = Paths.get(filename);
 
     //  creating method to create a directory
-    static void createDirectory(Path directoryName){
-        try{
-            if(Files.notExists(directoryName)){
+    static void createDirectory(Path directoryName) {
+        try {
+            if (Files.notExists(directoryName)) {
                 Files.createDirectory(directoryName);
             }
-        }catch(IOException iox){
+        } catch (IOException iox) {
             iox.printStackTrace();
         }
     }
 
     //  creating method to create a file
-    static void createFile(Path directoryName, Path dataFile){
-        try{
-            if(Files.notExists(directoryName)){
+    static void createFile(Path directoryName, Path dataFile) {
+        try {
+            if (Files.notExists(directoryName)) {
                 Files.createDirectory(directoryName);
             }
-            if(!Files.exists(dataFile)){
+            if (!Files.exists(dataFile)) {
                 Files.createFile(dataFile);
             }
-        }catch (IOException iox){
+        } catch (IOException iox) {
             iox.printStackTrace();
         }
     }
 
     //  method to write in file
-    static void writeOnFile(Path dataFile){
-        try{
-            if(Files.exists(dataFile)){
+    static void writeOnFile(Path dataFile) {
+        try {
+            if (Files.exists(dataFile)) {
                 Files.write(dataFile, Arrays.asList("A.J. Brown | 649-555-5555"));
             }
-        }catch(IOException iox){
+        } catch (IOException iox) {
+            iox.printStackTrace();
+        }
+    }
+
+    //  method to add to file
+    static void addToFile(Path dataFile, List<String> listName) {
+        try {
+            if (Files.exists(dataFile)) {
+                Files.write(dataFile, listName, StandardOpenOption.APPEND);
+            }
+        } catch (IOException iox) {
             iox.printStackTrace();
         }
     }
 
     //  method to list contacts
-    static void listContacts(){}
-    static void listContacts(Path fileName){
-    try{
-        List<String> contacts = Files.readAllLines(fileName);
-    }catch(IOException iox){
-        iox.printStackTrace();
+    static void listContacts() {
     }
+
+    static void listContacts(Path fileName) {
+        try {
+            List<String> contacts = Files.readAllLines(fileName);
+            for(String contact : contacts){
+                System.out.println(contact);
+            }
+            System.out.println(contacts);
+        } catch (IOException iox) {
+            iox.printStackTrace();
+        }
 
     }
 
     public static void main(String[] args) {
         Input user1 = new Input();
-        String directory = user1.getString("Name your Directory");
+//        String directory = user1.getString("Name your Directory");
 //        String directory = "data";
+        String directory = "TitansContacts";
         String filename = "contacts.txt";
 
         Path dataDirectory = Paths.get(directory);
         Path dataFile = Paths.get(directory, filename);
 
 //        createDirectory(dataDirectory);
-        createFile(dataDirectory, dataFile);
-        writeOnFile(dataFile);
-
+//        createFile(dataDirectory, dataFile);
+//        writeOnFile(dataFile);
+//        addToFile(dataFile, Arrays.asList("Julio Jones | 2105555550", "Ryan Tannehill | 2105555551"));
+        listContacts(dataFile);
     }
 }
