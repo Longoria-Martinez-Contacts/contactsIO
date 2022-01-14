6f74c4b3f1c3bab6ce574fee31526597b07d418e
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-01-14 17:09:31 -0600

    created Contact class with constructors/getters/setters

--- src/person/Contact.java
+++ src/person/Contact.java
@@ -1,2 +1,38 @@
-package person;public class Contact {
+package person;
+
+public class Contact {
+    //  FIELDS
+    String name;
+    int phone;
+
+    //  CONSTRUCTOR(S)
+
+    public Contact() {
+    }
+
+    public Contact(String name, int phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    //  METHODS
+        //  GETTERS/SETTERS
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+
 }
